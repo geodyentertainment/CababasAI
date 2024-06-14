4bd12cedea91a8ec53e183deaf671649b4ca7358
Wilson <wilsonyu657@gmail.com>
2024-06-14 03:27:55 -0400

    Created function for flag checking

--- cababas.py
+++ cababas.py
@@ -111,7 +111,7 @@ class CababasBot(discord.Client):
             await os_manager.resources.update_files()
             cons.task_completed(f'Resource files updated.               ')
         except Exception as e:
-            cons.error(f'An error occurred while updating resource files: {str(e)}.')
+            cons.error(f'An error occurred while updating resource files: {str(e)}. Bot will be stopped.')
             self.ready = False
             await self.stop()
             return
@@ -123,10 +123,7 @@ class CababasBot(discord.Client):
                 await self.tree.sync(guild=guild)
             cons.task_completed(f'Command tree synced to {str(len(os_manager.WHITELISTED_GUILDS))} guild(s).')
         except Exception as e:
-            cons.error(f'An error occurred while syncing app commands: {str(e)}.')
-            self.ready = False
-            await self.stop()
-            return
+            cons.error(f'An error occurred while syncing app commands: {str(e)}. Bot will continue to run.')
         
         self.ready = True
         cons.line()
@@ -146,14 +143,22 @@ class CababasBot(discord.Client):
         is_dm = (message.guild == None)
         content = message.clean_content
         guild = message.guild
+        
+        if sender.id in self.debounce:
+            await message.reply(f'u alweady running command {choice(faces.CONFUSED)}',delete_after=5.0)
+            return
+        
+        self.debounce.append(sender.id)
 
         if content.startswith('cab '):
             if is_dm:
                 await message.reply('sowwy :( no dms pls',delete_after=5.0)
+                self.debounce.remove(sender.id)
                 return
             
             if not self.enabled:
                 await message.reply('sowwy :( commands disable rn',delete_after=5.0)
+                self.debounce.remove(sender.id)
                 return
             
             actual_content = content.replace('cab ', '')
@@ -168,7 +173,7 @@ class CababasBot(discord.Client):
                     await message.reply(response,mention_author=False)
                 else:
                     await message.reply(response,mention_author=False,delete_after=10.0)    
-            return
+        self.debounce.remove(sender.id)
         
     def create_slash_commands(self) -> None:
         @self.tree.command(
@@ -181,22 +186,32 @@ class CababasBot(discord.Client):
                 }
         )
         async def toggle_commands(interaction:discord.Interaction, choice:bool):
-            if not self.ready:
-                await interaction.response.send_message('hold on pls', ephemeral=True)
-                return
-            
-            if interaction.user.id in self.debounce:
-                await interaction.response.send_message('u alweady running command', ephemeral=True, delete_after=5.0)
-                return
+            if not await self.check_flags(interaction,True): return
             self.debounce.append(interaction.user.id)
+            
+            self.enabled = choice
 
-            if await os_manager.resources.settings.discord.is_manager(interaction.user.id):
-                self.enabled = choice
+            cons.log(f'{interaction.user.name}-({interaction.user.id}) is accessing "toggle-commands" with a status of {str(choice)}')
+            await interaction.response.send_message(f'Command status is now [{choice}]', ephemeral=True)
+            self.debounce.remove(interaction.user.id)
+            
+        @self.tree.command(
+                name='toggle-commands-ai',
+                description='Toggle AI on/off GLOBALLY.',
+                guilds=self.get_whitelisted_guilds(),
+                extras={
+                    'on':True,
+                    'off':False
+                }
+        )
+        async def toggle_commands_ai(interaction:discord.Interaction, choice:bool):
+            if not await self.check_flags(interaction,True): return
+            self.debounce.append(interaction.user.id)
+            
+            await os_manager.resources.settings.ai_settings.set_enabled(choice)
 
-                cons.log(f'{interaction.user.name}-({interaction.user.id}) is accessing "toggle-commands" with a status of {str(choice)}')
-                await interaction.response.send_message(f'Command status is now [{choice}]', ephemeral=True)
-            else:
-                await interaction.response.send_message('You do not have permission to use this command.', ephemeral=True)
+            cons.log(f'{interaction.user.name}-({interaction.user.id}) is accessing "toggle-commands-ai" with a status of {str(choice)}')
+            await interaction.response.send_message(f'AI status is now [{choice}]', ephemeral=True)
             self.debounce.remove(interaction.user.id)
 
         @self.tree.command(
@@ -205,17 +220,7 @@ class CababasBot(discord.Client):
                 guilds=self.get_whitelisted_guilds()
         )
         async def rng_command(interaction:discord.Interaction):
-            if not self.ready:
-                await interaction.response.send_message('hold on pls', ephemeral=True)
-                return
-            
-            if not self.enabled:
-                await interaction.response.send_message('sowwy dev say no command rn :(', ephemeral=True)
-                return
-            
-            if interaction.user.id in self.debounce:
-                await interaction.response.send_message('u alweady running command', ephemeral=True, delete_after=5.0)
-                return
+            if not await self.check_flags(interaction,False): return
             self.debounce.append(interaction.user.id)
             
             user = interaction.user
@@ -244,17 +249,7 @@ class CababasBot(discord.Client):
                 guilds=self.get_whitelisted_guilds()
         )
         async def rng_view_rank(interaction:discord.Interaction):
-            if not self.ready:
-                await interaction.response.send_message('hold on pls', ephemeral=True, delete_after=10.0)
-                return
-            
-            if not self.enabled:
-                await interaction.response.send_message('sowwy dev say no command rn :(', ephemeral=True, delete_after=10.0)
-                return
-            
-            if interaction.user.id in self.debounce:
-                await interaction.response.send_message('u alweady running command', ephemeral=True, delete_after=10.0)
-                return
+            if not await self.check_flags(interaction,False): return
             self.debounce.append(interaction.user.id)
             
             user = interaction.user
@@ -273,17 +268,7 @@ class CababasBot(discord.Client):
                 guilds=self.get_whitelisted_guilds()
         )
         async def rng_browse_ranks(interaction:discord.Interaction):
-            if not self.ready:
-                await interaction.response.send_message('hold on pls', ephemeral=True, delete_after=10.0)
-                return
-            
-            if not self.enabled:
-                await interaction.response.send_message('sowwy dev say no command rn :(', ephemeral=True, delete_after=10.0)
-                return
-            
-            if interaction.user.id in self.debounce:
-                await interaction.response.send_message('u alweady running command', ephemeral=True, delete_after=10.0)
-                return
+            if not await self.check_flags(interaction,False): return
             self.debounce.append(interaction.user.id)
             
             user = interaction.user
@@ -300,6 +285,26 @@ class CababasBot(discord.Client):
     async def stop(self) -> None:
         cons.error(f'Terminating process...')
         exit(0)
+        
+    async def check_flags(self, interaction:discord.Interaction, is_manager_command:bool) -> bool:
+        if not self.ready:
+                await interaction.response.send_message('hold on pls', ephemeral=True, delete_after=10.0)
+                return False
+            
+        if not self.enabled:
+            await interaction.response.send_message(f'sowwy dev say no command rn {choice(faces.SAD)}', ephemeral=True, delete_after=10.0)
+            return False
+        
+        if interaction.user.id in self.debounce:
+            await interaction.response.send_message(f'u alweady running command {choice(faces.CONFUSED)}', ephemeral=True, delete_after=10.0)
+            return False
+        
+        if is_manager_command:
+            if not await os_manager.resources.settings.discord.is_manager(interaction.user.id):
+                await interaction.response.send_message(f'u no alowed to use dis {choice(faces.SAD)}', ephemeral=True, delete_after=10.0)
+                return False
+        
+        return True
         
     def generate_random_activity(self) -> Activity:
         random_type = choice(DISCORD_ACTIVITY_TYPES)
