d5fbfe7285acf440284c39f3d5e353d0ef692fc9
wyu4 <wilsonyu657@gmail.com>
2024-11-18 21:04:40 -0500

    Working bot

---
+++ CababasBot/secrets.py
@@ -0,0 +1,11 @@
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API:str = environ.get('OPENAI_CABABAS_API_KEY', 'unknown')
+OPENAI_PROJ:str = environ.get('OPENAI_CABABAS_PROJECT', 'unknown')
+OPENAI_ORG:str = environ.get('OPENAI_CABABAS_ORGANIZATION', 'unknown')
+OPENAI_MOD:str = environ.get('OPENAI_CABABAS_MODEL', 'unknown')
+DISCORD_TOK:str = environ.get('DISCORD_CABABAS_TOKEN', 'unknown')
+DISCORD_TOK_TEST:str = environ.get('DISCORD_CABABAS_TOKEN_TEST', 'unknown')
